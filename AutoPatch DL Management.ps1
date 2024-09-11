7678658857b114bfe0b9f91e1ac120e5c0a70816
jrollmann <81248392+jrollmann@users.noreply.github.com>
2024-09-11 12:41:03 -0400

    Add files via upload

---
+++ AutoPatch DL Management.ps1
@@ -0,0 +1,47 @@
+#Connect to Graph
+Connect-MgGraph
+
+#Connect to Exchange Online
+Connect-ExchangeOnline
+
+#Define Device Group from Entra
+$devicegroup = "Labs AutoPilot Devices"
+
+#Define DL Group UPN
+$dlupn = "TestDL1@MyLiveOakLabs.onmicrosoft.com"
+
+#Grab Group ID
+$groupid = (Get-MgGroup -Filter "DisplayName eq '$devicegroup'").Id
+
+try{
+
+    #Get devices in the specified Entra Group
+    $groupMembers = Get-MgGroupMember -GroupId $groupid -All
+
+    #Create array to hold primary emails
+    $primaryEmails = @()
+
+    foreach ($member in $groupMembers) {
+        #Grab device id
+        $deviceid = (Get-MgDevice -DeviceId $member.Id).DeviceId
+
+        #Grab primary email from Intune
+        $primaryEmail = (Get-MgDeviceManagementManagedDevice -Filter "AzureAdDeviceId eq '$deviceid'").EmailAddress
+
+        if($primaryEmail) {
+            #Add primary email to array
+            $primaryEmails += $primaryEmail
+        }
+
+
+    }
+
+    #Remove duplicates if needed
+    $primaryEmails = $primaryEmails | Sort-Object -Unique
+
+
+    #Update distribution group with new list of emails
+    Update-DistributionGroupMember -Identity "$dlupn" -Members $primaryEmails -Confirm:$false
+
+} catch {Write-Error "Failed to process group members: $_" 
+}
